679d327f225e6d0078a2d535a3627d14545948d5
Zaher Mousa <zaher.mousa@nb-zmousa2.local>
2023-11-15 23:36:47 +0100

    add google translator

---
+++ app/pipeline/stages/translate/translate.py
@@ -0,0 +1,17 @@
+from deep_translator import GoogleTranslator
+
+
+class TranslateStage:
+    google_translator_ar = GoogleTranslator(source='en', target='ar')
+    google_translator_en = GoogleTranslator(source='ar', target='en')
+    
+    def translate_ar(self, input_text: str) -> str:
+        translated_text = self.google_translator_ar.translate(input_text)
+        print(f"translated_text: {translated_text}")
+        return translated_text
+    
+    def translate_en(self, input_text: str) -> str:
+        translated_text = self.google_translator_en.translate(input_text)
+        print(f"translated_text: {translated_text}")
+        return translated_text
+
