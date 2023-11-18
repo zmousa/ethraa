949508717ad8d81129a134c13a6c990c6fe447ed
faditaqialdin <150848368+faditaqialdin@users.noreply.github.com>
2023-11-18 15:57:40 +0100

    Extracting bard environment variables

--- app/pipeline/stages/llm/llm_bard.py
+++ app/pipeline/stages/llm/llm_bard.py
@@ -1,9 +1,18 @@
 from bardapi import BardCookies
 import json
+import os
 
 class LLMBard:
 
-    def __init__(self, bard_cookie_dict):
+    def __init__(self):
+        # Visit https://bard.google.com/
+        # Open the console
+        # Session: Application → Cookies → Copy the following values to environment variables
+        bard_cookie_dict = {
+            "__Secure-1PSID": os.getenv("bardSecure1PSID"),
+            "__Secure-1PSIDTS": os.getenv("bardSecure1PSIDTS"),
+            "__Secure-1PSIDCC": os.getenv("bardSecure1PSIDCC"),
+        }
         self.bard = BardCookies(cookie_dict=bard_cookie_dict)
     
     def describe(self, word):
@@ -13,17 +22,18 @@ class LLMBard:
         return result_dict
         
 
-
-
-# Visit https://bard.google.com/
-# Open the console
-# Session: Application → Cookies → Copy the following values
-bard_cookie_dict = {
-    "__Secure-1PSID": "",
-    "__Secure-1PSIDTS": "",
-    "__Secure-1PSIDCC": ""
-}
-
-word = "حب"
-llmBard = LLMBard(bard_cookie_dict)
+word = "فرح"
+llmBard = LLMBard()
 print(llmBard.describe(word))
+# {
+#     "word": "فرح",
+#     "meaning": "السعادة والبهجة والسرور",
+#     "synonyms": ["ابتهاج", "بهجة", "سرور", "غبطة", "مسرة"],
+#     "antonyms": ["حزن", "هم", "غم", "كآبة", "أسى", "ابتأس", "اغتم", "اكتأب", "شقي"],
+#     "examples": [
+#         "فرحتُ بنجاحه",
+#         "أفرحني خبر تخرجك",
+#         "فرحتُ كثيراً عندما رأيتك"
+#     ]
+# }
+
